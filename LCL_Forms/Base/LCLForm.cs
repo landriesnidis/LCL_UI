f127514be220611b858c838f793e48143588efca
landriesnidis <landriesnidis@yeah.net>
2020-08-24 10:58:21 +0800

    窗体用双缓冲绘制窗口的所有子控件

--- LCL_Forms/Base/LCLForm.cs
+++ LCL_Forms/Base/LCLForm.cs
@@ -30,6 +30,18 @@ namespace Landriesnidis.LCL_Forms.Base
             mmc.Form = this;
             mmc.Add(new Control[] { labTitle,panelBase });
             this.StartPosition = FormStartPosition.CenterScreen;
+            // 获取执行程序的图标
+            Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000; // 用双缓冲绘制窗口的所有子控件
+                return cp;
+            }
         }
 
         /// <summary>
